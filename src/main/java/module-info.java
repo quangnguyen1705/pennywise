55f39d6e383d985a2cc66b937b2979ffc3a7c7bf
Matthew Tran <matthewtran0122@gmail.com>
2024-10-26 01:01:32 -0700

    Table added to main page with test data. TODO: get arraylist containing all objects from the db

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module sjsu.edu.pennywise {
 	 requires javafx.controls;
 	 requires javafx.fxml;
 	 requires java.sql;
+	 requires javafx.base;
 	 exports sjsu.edu.pennywise;
 	 exports sjsu.edu.pennywise.Controllers;
 	 opens sjsu.edu.pennywise.Controllers to javafx.fxml;
--- src/main/java/sjsu/edu/pennywise/Controllers/MainController.java
+++ src/main/java/sjsu/edu/pennywise/Controllers/MainController.java
@@ -1,34 +1,56 @@
 package sjsu.edu.pennywise.Controllers;
+/**
+ * @author Matthew Tran
+ */
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.TableColumn;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import sjsu.edu.pennywise.Account;
 
-public class MainController{
+public class MainController implements Initializable{
 	
 	private Stage stage;
 	private Scene scene;
 	
 	@FXML
-	private ListView<String> AccListView;
-	
-	//dummy list, TODO: get list from DB 
-	String[] accounts = {"acc1", "acc2", "acc3"};
-	
-	
+	private TableView<Account> AccListView;
 	@FXML
-	public void initialize() {
+	private TableColumn<Account, String> AccName;
+	@FXML
+	private TableColumn<Account, Double> OpBalance;
+	@FXML
+	private TableColumn<Account, String> OpDate;
+	
+	//Test Data
+	//TODO: get the list of accounts
+	ObservableList<Account> list = FXCollections.observableArrayList(
+			
+			new Account("ahvdhwad", "TestChecking", 23.33, "12/12/2024")
+			
+			);
+			
+	@FXML
+	public void initialize(URL url, ResourceBundle rb) {
 		// TODO Auto-generated method stub
-		if (AccListView != null) {
-			AccListView.getItems().addAll(accounts);
-		}
+		AccName.setCellValueFactory(new PropertyValueFactory<>("bankName"));
+		OpBalance.setCellValueFactory(new PropertyValueFactory<>("balance"));
+		OpDate.setCellValueFactory(new PropertyValueFactory<>("date"));
 		
+		AccListView.setItems(list);
 		
 	}
 	
