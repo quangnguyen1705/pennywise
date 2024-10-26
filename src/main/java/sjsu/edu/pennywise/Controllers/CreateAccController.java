032dd4cedef526fc67b704cb0914538355208bba
your name <nhimduon1505@gmail.com>
2024-10-26 01:28:07 -0700

    CreateAccUpdate

--- src/main/java/sjsu/edu/pennywise/Controllers/CreateAccController.java
+++ src/main/java/sjsu/edu/pennywise/Controllers/CreateAccController.java
@@ -1,11 +1,16 @@
 package sjsu.edu.pennywise.Controllers;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -22,4 +27,48 @@ public class CreateAccController {
 		stage.setScene(scene);
 		stage.show();
 	}
+	
+	public void initialize() { //stop people enter on Date Picker
+	OpeningDate.getEditor().setDisable(true);
+	OpeningDate.getEditor().setOpacity(1);
+	}
+	
+	
+	@FXML
+	private TextField AccountName;	
+	@FXML
+	private TextField OpeningBalance;
+	@FXML
+	private DatePicker OpeningDate;
+	@FXML
+	private Button myButton;
+	@FXML
+	private Label MyLabel;
+	
+	String accName;
+	Integer openBalance;
+	LocalDate openDate;
+	
+
+	public void submit(ActionEvent event) {
+		
+	    try {
+	        // Directly assign the String from the TextField
+	        openBalance = Integer.parseInt(OpeningBalance.getText());
+	        System.out.println(openBalance);
+
+	        openDate = OpeningDate.getValue();
+	        System.out.println(openDate);
+	        
+	    	accName = AccountName.getText();
+	        System.out.println(accName);
+
+
+	    }
+	    catch (Exception e) {
+	        MyLabel.setText("Enter Valid");
+	    }
+	}
 }
+
+	
