209a4eddcd618bd6d46ca8cc619617ab4d6397fa
Aditya Rao <aditya.k.rao@sjsu.edu>
2024-10-26 04:00:36 -0700

    Update schema.sql

--- src/main/resources/config/schema.sql
+++ src/main/resources/config/schema.sql
@@ -1,6 +1,7 @@
 --Accounts database in SQLLite
 CREATE TABLE IF NOT EXISTS accounts(
-	name TEXT PRIMARY KEY,
+	id TEXT PRIMARY KEY, -- this wont be shown to the user
+	bank_name TEXT NOT NULL, -- probably could have named this account_name instead, but this functionality still works
 	open_balance DOUBLE NOT NULL,  
-	open_date TEXT NOT NULL
+	open_date DATE NOT NULL
 );
