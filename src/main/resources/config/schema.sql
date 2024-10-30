89dabe1e9eb2a7e3e96946f9d402e6bdb2544744
Aditya Rao <aditya.k.rao@sjsu.edu>
2024-10-29 20:36:40 -0700

    transaction type schema

--- src/main/resources/config/schema.sql
+++ src/main/resources/config/schema.sql
@@ -5,3 +5,17 @@ CREATE TABLE IF NOT EXISTS accounts(
 	open_balance DOUBLE NOT NULL,  
 	open_date DATE NOT NULL
 );
+
+-- Transactions Database in SQLite
+CREATE TABLE IF NOT EXISTS transactions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    account_id TEXT NOT NULL,
+    transaction_type TEXT NOT NULL,
+    transaction_date DATE NOT NULL, 
+    transaction_description TEXT NOT NULL,
+    payment_amount DOUBLE NOT NULL DEFAULT 0,
+    deposit_amount DOUBLE NOT NULL DEFAULT 0,
+    CHECK ((payment_amount = 0 AND deposit_amount > 0) OR -- check that either payment_amount or deposit_amount is present, and not both
+           (payment_amount > 0 AND deposit_amount = 0)),
+    FOREIGN KEY (account_id) REFERENCES accounts(id) -- account_id has to reference an account in the accounts table 
+);
