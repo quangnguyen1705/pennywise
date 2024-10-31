1cbc8fe315924d108f99ce2a41fd8a602b3f51ef
Matthew Tran <matthewtran0122@gmail.com>
2024-10-30 22:13:48 -0700

    Partially finished AddTransaction controller, need to verify extra info with prof

--- src/main/java/sjsu/edu/pennywise/Controllers/AddTransactionController.java
+++ src/main/java/sjsu/edu/pennywise/Controllers/AddTransactionController.java
@@ -1,18 +1,66 @@
 package sjsu.edu.pennywise.Controllers;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import sjsu.edu.pennywise.Account;
+import sjsu.edu.pennywise.AccountList;
 
 public class AddTransactionController {
 	
 	private Stage stage;
 	private Scene scene;
 	
+	@FXML
+	private Label errMsg;
+	@FXML
+	private ChoiceBox<String> chosenAccount;
+	@FXML
+	private TextField transactionTypeInput;
+	@FXML
+	private DatePicker transactionDateInput;
+	@FXML
+	private TextField descriptionInput;
+	@FXML
+	private TextField transactionAmount;
+	@FXML
+	private TextField depositAmount;
+	
+	private AccountList accountList = new AccountList();
+	
+	public void initialize() {
+		// Set the current date as the default value
+		transactionDateInput.setValue(LocalDate.now());
+
+        // Prevent users from typing in the DatePicker
+		transactionDateInput.getEditor().setDisable(true);
+		transactionDateInput.getEditor().setOpacity(1);		
+		chosenAccount.getItems().add("--Choose an Account--");
+		chosenAccount.getItems().addAll(getAccountsByName(accountList.getList()));
+		chosenAccount.getSelectionModel().selectFirst();
+	}
+	
+	private ArrayList<String> getAccountsByName(ArrayList<Account> accList) {
+		// TODO Auto-generated method stub
+		ArrayList<String> list = new ArrayList<>();
+		for (Account acc: accList) {
+			list.add(acc.getBankName());
+		}
+		
+		return list;
+	}
+
 	public void switchToMain(ActionEvent event) throws IOException {
 	    FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Main.fxml"));
 	    AnchorPane root = loader.load();
@@ -30,6 +78,31 @@ public class AddTransactionController {
 		
 		
 	}
-	
+
+	public void newTransaction(ActionEvent event) {
+		errMsg.setText("");
+		//check for existing account
+		String accountName = chosenAccount.getValue();
+		if (accountName.equals("--Choose an Account--")) {
+			errMsg.setText("Please select an Account");
+			return;
+		}
+		//check transaction type
+		//TODO: check to see if the type is fetched from a list
+		
+		//get variable for transaction date
+		LocalDate date = transactionDateInput.getValue();
+		
+		//check for existing description
+		String description = descriptionInput.getText();
+		if (description.equals("")) {
+			errMsg.setText("Please enter description");
+			return;
+		}
+		
+		//check for existing transaction amount
+		//TODO: if transaction type is depositing money into an account, use deposit amount instead
+		
+	}
 
 }
