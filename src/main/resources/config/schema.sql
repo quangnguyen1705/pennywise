37af3bd38d4a7067836a3ee89d3fc41b2ac916d9
Aditya Rao <aditya.k.rao@sjsu.edu>
2024-11-02 13:56:16 -0700

    Update schema.sql

--- src/main/resources/config/schema.sql
+++ src/main/resources/config/schema.sql
@@ -18,4 +18,12 @@ CREATE TABLE IF NOT EXISTS transactions (
     CHECK ((payment_amount = 0 AND deposit_amount > 0) OR -- check that either payment_amount or deposit_amount is present, and not both
            (payment_amount > 0 AND deposit_amount = 0)),
     FOREIGN KEY (account_id) REFERENCES accounts(id) -- account_id has to reference an account in the accounts table 
+    FOREIGN KEY (transaction_type) REFERENCES transaction_types(type_name) --valid transaction type
 );
+
+
+-- Transaction Types database in sqlite
+CREATE TABLE IF NOT EXISTS transaction_types(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	type_name TEXT UNIQUE NOT NULL -- make sure it's a unqique name
+)
