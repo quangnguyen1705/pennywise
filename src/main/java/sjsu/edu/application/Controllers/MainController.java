cf5b0a8362b3d22cec3425b3407b72bc5587322e
Matthew Tran <matthewtran0122@gmail.com>
2024-11-16 12:27:56 -0800

    Buttons added to reach Scheduled Transactions pages

--- src/main/java/sjsu/edu/application/Controllers/MainController.java
+++ src/main/java/sjsu/edu/application/Controllers/MainController.java
@@ -85,8 +85,21 @@ public class MainController implements Initializable{
 		stage.show();
 		
 	}
-
-	
+	public void switchToAddSchedule(ActionEvent event) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("/views/AddScheduledTransaction.fxml"));
+		stage = (Stage)((javafx.scene.Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+		
+	}
+	public void switchToSchedule(ActionEvent event) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("/views/ScheduledTransaction.fxml"));
+		stage = (Stage)((javafx.scene.Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
 
 
 }
